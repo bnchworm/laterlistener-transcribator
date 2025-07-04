eec4059ee0b87afd68e6aac6c9236e272a948019
Michael <mvasilyev04@gmail.com>
2025-07-04 16:57:48 +0300

    Сделал функцию диаризации

---
+++ diarization.py
@@ -0,0 +1,22 @@
+from typing import Dict, List
+from pyannote.audio import Pipeline, Audio
+from dotenv import load_dotenv
+import os
+
+def diarize(audio_path: str) -> List[Dict]:
+    audio = Audio(sample_rate=16000)
+    load_dotenv('.env.example')
+    diarization_token = os.getenv('DIARIZATION_TOKEN')
+    
+    waveform, sample_rate = audio(audio_path)
+    pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1", 
+                                        use_auth_token=diarization_token)
+    diarization = pipeline({
+        "waveform": waveform,
+        "sample_rate": sample_rate
+    })
+
+    return [
+        {"start": segment.start, "end": segment.end, "speaker": speaker}
+        for segment, _, speaker in diarization.itertracks(yield_label=True)
+    ]
