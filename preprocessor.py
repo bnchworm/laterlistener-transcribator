5eb7a38b420aaa9c538d887362c5b38d13e34ae8
Volkov Ilia <O4iVidno@gmail.com>
2025-07-04 20:08:13 +0300

    Убраны данные о файле

--- preprocessor.py
+++ preprocessor.py
@@ -1,25 +1,11 @@
 from pydub import AudioSegment
 import os
-import wave
 
 def preprocess_audio(pfile):
     filename, ext = os.path.splitext(pfile)
     ofile = pfile if ext == '.wav' else f"{filename}.wav"
     if ext != ".wav":
         AudioSegment.from_file(pfile).export(ofile, format="wav")
-    
-    with wave.open(ofile, 'rb') as audiofile:
-        frame_rate = audiofile.getframerate()
-        n_frames = audiofile.getnframes()
-        duration = n_frames / float(frame_rate)
-        
-        return {
-            'filename': ofile,
-            'frame_rate': frame_rate,
-            'n_frames': n_frames,
-            'duration': duration
-        }
 
 path_to_file = "path"
-result = preprocess_audio(path_to_file)
-print(result)
+preprocess_audio(path_to_file)
