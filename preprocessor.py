e4ce8151426dee22178602ea66dcea127735c00c
Volkov Ilia <O4iVidno@gmail.com>
2025-07-04 16:12:23 +0300

    add function

---
+++ preprocessor.py
@@ -0,0 +1,26 @@
+from pydub import AudioSegment
+import os
+import wave
+
+def preprocess_audio(pfile):
+    audio = AudioSegment.from_file(pfile)
+    file = os.path.splitext(pfile)[0]
+    ofile = f"{file}.wav"
+    
+    audio.export(ofile, format="wav", parameters=["-ac", "1", "-ar", "16000"])
+    
+    with wave.open(ofile, 'rb') as audiofile:
+        frame_rate = audiofile.getframerate()
+        n_frames = audiofile.getnframes()
+        duration = n_frames / float(frame_rate)
+        
+        return {
+            'filename': ofile,
+            'frame_rate': frame_rate,
+            'n_frames': n_frames,
+            'duration': duration
+        }
+
+path_to_file = "path"
+result = preprocess_audio(path_to_file)
+print(result)
