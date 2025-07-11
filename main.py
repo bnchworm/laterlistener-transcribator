5135f46bec0760c42cb9ffda88769df1cd13b323
Nikita Utyugov <neavinstasi@icloud.com>
2025-07-11 03:36:29 +0300

    Добавлено удаление файлов после использования. Использована кроссплатформенная генерация путей к файлам

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from urllib.request import urlretrieve
 from schema import TaskStatus
 import time
 import json
+import os
 from supabase_client import upload_file_to_supabase
 
 
@@ -25,17 +26,25 @@ while True:
     if set_task_status(task.id, TaskStatus.running) == 0:
         continue
 
-    path_to_audio = f'{PATH_TO_AUDIO_FILES}/{task.file_name}'
+    path_to_audio = os.path.join(PATH_TO_AUDIO_FILES, task.file_name)
     urlretrieve(task.file_url, path_to_audio)
     diarization_result = diarize(path_to_audio)
     transcription_result = transcription(path_to_audio)
     align_result = align_speakers_with_text(transcription_result, diarization_result)
 
     base_file_name = task.id
-    with open(f'{PATH_TO_TRANSCRIPTIONS}/{base_file_name}.json', 'w', encoding='utf8') as f:
+    transcription_file_path = os.path.join(PATH_TO_TRANSCRIPTIONS, f'{base_file_name}.json')
+    with open(transcription_file_path, 'w', encoding='utf8') as f:
         json.dump(align_result, f, ensure_ascii=False)
 
     # Загружаем результат в Supabase и сохраняем ссылку
-    public_url = upload_file_to_supabase(f'{PATH_TO_TRANSCRIPTIONS}/{base_file_name}.json', 'transcriptions', f'{base_file_name}.json', 'application/json')
+    public_url = upload_file_to_supabase(transcription_file_path, 'transcriptions', f'{base_file_name}.json', 'application/json')
     set_task_result_url(task.id, public_url)
     set_task_status(task.id, TaskStatus.finished)
+    
+    # Удаляем временные файлы
+    try:
+        os.remove(path_to_audio)
+        os.remove(transcription_file_path)
+    except OSError as e:
+        print(f"Ошибка при удалении файлов: {e}")
