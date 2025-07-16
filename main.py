142f310f63dc450990feb545344c61eb60153600
Nikita Utyugov <neavinstasi@icloud.com>
2025-07-16 23:31:56 +0300

    Исправлена логика распараллеливания кода

--- main.py
+++ main.py
@@ -26,29 +26,30 @@ async def main():
                 await asyncio.sleep(0.5)
                 continue
 
-            if await asyncio.create_task(set_task_status(task.id, TaskStatus.running)) == 0:
+            if set_task_status(task.id, TaskStatus.running) == 0:
                 continue
 
             path_to_audio = os.path.join(PATH_TO_AUDIO_FILES, task.file_name)
-            await asyncio.create_task(urlretrieve(task.file_url, path_to_audio))
-            diarization_result = await asyncio.to_thread(diarize, path_to_audio)
-            transcription_result = await asyncio.to_thread(transcription,path_to_audio)
-            align_result = await asyncio.to_thread(align_speakers_with_text, transcription_result, diarization_result)
+            urlretrieve(task.file_url, path_to_audio)
+            diarization_task = asyncio.create_task(asyncio.to_thread(diarize, path_to_audio))
+            transcription_task = asyncio.create_task(asyncio.to_thread(transcription, path_to_audio))
+            diarization_result, transcription_result = await asyncio.gather(diarization_task, transcription_task)
+            align_result = align_speakers_with_text(transcription_result, diarization_result)
 
             base_file_name = task.id
             transcription_file_path = os.path.join(PATH_TO_TRANSCRIPTIONS, f'{base_file_name}.json')
             async with aiofiles.open(transcription_file_path, 'w', encoding='utf8') as f:
-                await asyncio.create_task(json.dump(align_result, f, ensure_ascii=False))
+                json.dump(align_result, f, ensure_ascii=False)
 
             # Загружаем результат в Supabase и сохраняем ссылку
-            public_url = await asyncio.create_task(upload_file_to_supabase(transcription_file_path, 'transcriptions', f'transcriptions/{base_file_name}.json', 'application/json'))
-            await asyncio.create_task(set_task_result_url(task.id, public_url))
-            await asyncio.create_task(set_task_status(task.id, TaskStatus.finished))
+            public_url = upload_file_to_supabase(transcription_file_path, 'transcriptions', f'transcriptions/{base_file_name}.json', 'application/json')
+            set_task_result_url(task.id, public_url)
+            set_task_status(task.id, TaskStatus.finished)
             
             # Удаляем временные файлы
             try:
-                aiofiles.os.remove(path_to_audio)
-                aiofiles.os.remove(transcription_file_path)
+                await aiofiles.os.remove(path_to_audio)
+                await aiofiles.os.remove(transcription_file_path)
             except OSError as e:
                 print(f"Ошибка при удалении файлов: {e}")
         except RuntimeWarning as e:
